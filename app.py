35c6ffdc8863a84919766790fa4e1afe81559857
wedagechathura-lang <wedagechathura@gmail.com>
2026-01-25 15:26:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -256,7 +256,7 @@ if image_file is not None:
             with col1:
                 st.subheader("Stress Heatmap")
                 # HERE IS YOUR BGR CHANNEL REQUEST:
-                st.image(result_img, channels="BGR", use_container_width=True)
+                st.image(result_img, channels="RGB", use_container_width=True)
             
             with col2:
                 st.subheader("Data")
